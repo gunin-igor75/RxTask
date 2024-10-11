343f72059b0cd8d9fa8836b2152cc4075b86494a
Igor Gunin <gunin-igor75@yandex.ru>
2024-10-11 17:39:00 +0600

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.jetbrains.kotlin.android)
+}
+
+android {
+    namespace = "com.github.gunin_igor75.rxtask"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.github.gunin_igor75.rxtask"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    implementation(libs.rxjava.core)
+    implementation(libs.rxjava.kotlin)
+    implementation(libs.rxjava.android)
+
+    implementation(libs.okhttp3.logging)
+    implementation(libs.retrofit.core)
+    implementation(libs.retrofit.gson)
+    implementation(libs.retrofit.converter.rxjava)
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/github/gunin_igor75/rxtask/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.gunin_igor75.rxtask.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.gunin_igor75.rxtask.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
